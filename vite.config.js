8b6f1024e8b622583e9e0367899e770d63d6de79
repositorioDahiana <brahiandiazladi@gmail.com>
2026-09-24 07:36:48 -0500

    implemetacion de vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,34 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno según el modo (development, production)
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  // Si existe VITE_API_URL la usa, de lo contrario usa el localhost por defecto
+  const targetUrl = env.VITE_API_URL || 'http://localhost:8000';
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: [
+        'pet-shop-production-8f5c.up.railway.app',
+        '.up.railway.app'
+      ],
+      proxy: {
+        '/api': {
+          target: targetUrl,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+    preview: {
+      allowedHosts: [
+        'pet-shop-production-8f5c.up.railway.app',
+        '.up.railway.app'
+      ]
+    }
+  }
 })
